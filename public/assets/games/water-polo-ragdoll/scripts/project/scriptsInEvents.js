ea5be69db68861df2c6b942d10be7094d3b499d3
smxrtfoloo <smartfoloo@gmail.com>
2024-10-29 00:03:24 -0500

    new games

---
+++ public/assets/games/water-polo-ragdoll/scripts/project/scriptsInEvents.js
@@ -0,0 +1,29 @@
+
+
+
+const scriptsInEvents = {
+
+	async Game_Event20_Act4(runtime, localVars)
+	{
+		self.addEventListener('keydown', ev => {
+		    if (['ArrowDown', 'ArrowUp', ' '].includes(ev.key)) {
+		        ev.preventDefault();
+		    }
+		});
+		self.addEventListener('wheel', ev => ev.preventDefault(), { passive: false });
+	},
+
+	async Game_Event20_Act6(runtime, localVars)
+	{
+		c3canvas.style.webkitUserSelect=c3canvas.style.msUserSelect=c3canvas.style.userSelect="none";
+	},
+
+	async Sdk_Event40_Act1(runtime, localVars)
+	{
+		PokiSDK.customEvent(localVars.Game, localVars.Segment, { label: localVars.Level, value: localVars.Value });
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
