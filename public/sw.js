b564a080048e46c3eb0e4674491700ca119b21a1
smxrtfoloo <smartfoloo@gmail.com>
2024-10-31 22:10:33 -0500

    remove scramjet (for now)

--- public/sw.js
+++ public/sw.js
@@ -3,16 +3,9 @@ importScripts('/dynamic/dynamic.worker.js');
 importScripts('/uv/uv.bundle.js');
 importScripts('/uv/uv.config.js');
 importScripts(__uv$config.sw || '/uv/uv.sw.js');
-importScripts(
-  '/scramjet/scramjet.codecs.js',
-  '/scramjet/scramjet.config.js',
-  '/scramjet/scramjet.shared.js',
-  '/scramjet/scramjet.worker.js'
-);
 
 const uv = new UVServiceWorker();
 const dynamic = new Dynamic();
-const scramjet = new ScramjetServiceWorker();
 
 self.addEventListener('fetch', event => {
   if (event.request.url.startsWith(location.origin + '/service/dynamic/')) {
@@ -26,9 +19,5 @@ self.addEventListener('fetch', event => {
   if (event.request.url.startsWith(location.origin + '/service/aero/')) {
     event.respondWith(uv.fetch(event));
     console.log("Tried to fetch Aero");
-  }
-  if (event.request.url.startsWith(location.origin + '/service/scramjet/')) {
-    event.respondWith(scramjet.fetch(event));
-    console.log("Tried to fetch Scramjet");
   }
 });
